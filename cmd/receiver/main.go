cf00b0cae1973832cac7d8fc2c672e0efe265d8a
SayYoungMan <nickte89@gmail.com>
2021-11-09 21:42:50 +0000

    feat(receiver): receiver gets items in playlist

--- cmd/receiver/main.go
+++ cmd/receiver/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/SayYoungMan/gitmuzik-backend/internal/receiver"
+import (
+	"fmt"
+
+	"github.com/SayYoungMan/gitmuzik-backend/internal/receiver"
+)
+
+const (
+	dailyPlaylistID string = "PLXzLX2ct6ysab-Gy0b1Xrm9Ka-Pg-yqmR"
+)
 
 func main() {
-	receiver.GetPlaylist()
+	items := receiver.GetPlaylistItems(dailyPlaylistID)
+	for _, item := range items {
+		fmt.Println(item.Snippet.Title)
+	}
 }
--- internal/receiver/receiver.go
+++ internal/receiver/receiver.go
@@ -2,32 +2,27 @@ package receiver
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	"google.golang.org/api/option"
 	"google.golang.org/api/youtube/v3"
 )
 
-const (
-	dailyPlaylistID string = "PLXzLX2ct6ysab-Gy0b1Xrm9Ka-Pg-yqmR"
-)
-
-func GetPlaylist() {
+func GetPlaylistItems(playlistID string) []*youtube.PlaylistItem {
 	client := getClient(youtube.YoutubeReadonlyScope)
 	ctx := context.Background()
 
 	service, err := youtube.NewService(ctx, option.WithHTTPClient(client))
 	if err != nil {
-		log.Fatalf("Error creating Youtube client: %v", err)
+		log.Fatalf("Error creating Youtube service: %v", err)
 	}
+	itemService := youtube.NewPlaylistItemsService(service)
 
-	call := service.Playlists.List([]string{"snippet", "contentDetails"})
-	call = call.Id(dailyPlaylistID)
+	call := itemService.List([]string{"snippet"})
+	call = call.PlaylistId(playlistID)
+	call = call.MaxResults(10000)
 	response, err := call.Do()
 	handleError(err, "")
 
-	for _, playlist := range response.Items {
-		fmt.Println(playlist.Id, ": ", playlist.Snippet.Title)
-	}
+	return response.Items
 }
