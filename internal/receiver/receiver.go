22c7d397ed4a8e4ffb746e0bfbeedc209d0f55ba
SayYoungMan <nickte89@gmail.com>
2021-11-09 21:00:11 +0000

    fix: deprecated youtube.New changed to youtube.NewService

--- internal/receiver/receiver.go
+++ internal/receiver/receiver.go
@@ -1,22 +1,29 @@
 package receiver
 
 import (
+	"context"
 	"fmt"
 	"log"
 
+	"google.golang.org/api/option"
 	"google.golang.org/api/youtube/v3"
+)
+
+const (
+	dailyPlaylistID string = "PLXzLX2ct6ysab-Gy0b1Xrm9Ka-Pg-yqmR"
 )
 
 func GetPlaylist() {
 	client := getClient(youtube.YoutubeReadonlyScope)
+	ctx := context.Background()
 
-	service, err := youtube.New(client)
+	service, err := youtube.NewService(ctx, option.WithHTTPClient(client))
 	if err != nil {
 		log.Fatalf("Error creating Youtube client: %v", err)
 	}
 
 	call := service.Playlists.List([]string{"snippet", "contentDetails"})
-	call = call.Id("PLXzLX2ct6ysab-Gy0b1Xrm9Ka-Pg-yqmR")
+	call = call.Id(dailyPlaylistID)
 	response, err := call.Do()
 	handleError(err, "")
 
