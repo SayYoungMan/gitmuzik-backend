485c7a39ce8beebfa95e08f67f56f2afd1bd66e1
Seyoung Lee <nickte89@gmail.com>
2021-11-13 19:38:02 +0000

    feat(connector): connection to S3 (#9)
    * feat: connector for db connection
    
    * feat(connector): connection to S3

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,8 +4,7 @@ import (
 	"fmt"
 
 	"github.com/SayYoungMan/gitmuzik-backend/internal/connector"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/aws"
 )
 
 const (
@@ -14,27 +13,39 @@ const (
 
 func main() {
 	// receiver.ReceiveAndSavePlaylistItems(dailyPlaylistID, "test.json")
-	client := connector.ConnectToDB()
 
-	input := &dynamodb.ListTablesInput{}
-	tables, err := client.ListTables(input)
+	// Check connection to db
+	// client := connector.ConnectToDB()
+
+	// input := &dynamodb.ListTablesInput{}
+	// tables, err := client.ListTables(input)
+	// if err != nil {
+	// 	if aerr, ok := err.(awserr.Error); ok {
+	// 		switch aerr.Code() {
+	// 		case dynamodb.ErrCodeInternalServerError:
+	// 			fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
+	// 		default:
+	// 			fmt.Println(aerr.Error())
+	// 		}
+	// 	} else {
+	// 		// Print the error, cast err to awserr.Error to get the Code and
+	// 		// Message from an error.
+	// 		fmt.Println(err.Error())
+	// 	}
+	// 	return
+	// }
+
+	// for _, n := range tables.TableNames {
+	// 	fmt.Println(*n)
+	// }
+
+	// Check connection to s3
+	client := connector.ConnectToS3()
+	res, err := client.ListBuckets(nil)
 	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case dynamodb.ErrCodeInternalServerError:
-				fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
-			default:
-				fmt.Println(aerr.Error())
-			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			fmt.Println(err.Error())
-		}
-		return
+		fmt.Printf("Unable to list buckets, %v", err)
 	}
-
-	for _, n := range tables.TableNames {
-		fmt.Println(*n)
+	for _, b := range res.Buckets {
+		fmt.Printf("* %s created on %s\n", aws.StringValue(b.Name), aws.TimeValue(b.CreationDate))
 	}
 }
